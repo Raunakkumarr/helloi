524bf1e0070b1005561e811c3b9637af097b6124
Raunakkumarr <52267072+Raunakkumarr@users.noreply.github.com>
2020-09-21 23:59:40 +0545

    Responsive for mobiles

--- scroll on top button.js
+++ scroll on top button.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 || screen.width>768) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && screen.width>768) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
